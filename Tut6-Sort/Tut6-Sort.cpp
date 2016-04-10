c69c7cb769974d263b956d29ce67cb267fdd0d7c
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-04-10 15:57:40 +0200

    completed program and tested it with different data types as shown

--- Tut6-Sort/Tut6-Sort.cpp
+++ Tut6-Sort/Tut6-Sort.cpp
@@ -1,10 +1,10 @@
 // Cheyenne A Pillay
 // 214513360
 
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
- // create template 
+ // create template to sort
 template <typename Tmpl8>
 
 // create a sort function  to sort an array given the array of integers and the array size
@@ -31,23 +31,52 @@ void sortArr(Tmpl8 arrIntegers[], int arrSize){
 
 }
 
-
-void disp(int arr[], int size){
+ // new template to display
+template <typename T>
+void disp(T arr[], int size) {
 	for (int i = 0; i < size; i++){
-		cout << arr[i];
-		cout << endl;
+		cout << arr[i] << endl;
+		
 	}
 }
 
 
  // main method
 int main(){
+
+	// testing the template program
+	cout << " The following arrays below are sorted in Decending order" << endl; 
+	cout << endl;
 	// create an array to populate the function with
 	int array1[5] = {1,5,4,6,2};
-	cout << "unsorted array" << endl;
+	cout << "unsorted Integer array" << endl;
 	disp(array1, 5);
 	cout << endl;
 	sortArr(array1, 5);
-	cout << "sorted array" << endl;
+	cout << "sorted Integer array" << endl;
 	disp(array1, 5);
+	cout <<" ________________________________________________________________________" << endl;
+
+	char array2[5] = { 'a', 'm', 'z', 'r', 'q' };
+	cout << "unsorted character array" << endl;
+	disp(array2, 5);
+	cout << endl;
+	sortArr(array2, 5);
+	cout << "sorted character array" << endl;
+	disp(array2, 5);
+
+	cout << " ________________________________________________________________________" << endl;
+
+	// create a double array to test the methods with 
+	double array3[5] = { 1.52, 1.36, 0.25, 0.85, 2.15 };
+	cout << "unsorted decimal array" << endl;
+	disp(array3, 5);
+	cout << endl;
+	sortArr(array3, 5);
+	cout << "sorted decimal array" << endl;
+	disp(array3, 5);
+
+	cout << " ________________________________________________________________________" << endl;
+	
+	cout << "end of program" << endl;
 }
