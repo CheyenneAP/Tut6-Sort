dec2d402713bd0352fe34af252cf29828e00de63
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-04-10 15:02:03 +0200

    setting up the structure of the program

---
+++ Tut6-Sort/Tut6-Sort.cpp
@@ -0,0 +1,20 @@
+// Cheyenne A Pillay
+// 214513360
+
+#include <iostream> 
+using namespace std;
+
+ // create template 
+template <typename Tmpl8>
+
+// create a sort function  to sort an array
+
+void sortArr(Tmpl8 arrIntegers[], int arrSize){
+
+}
+
+
+ // main method
+int main(){
+
+}
