8049dd7e6e90e6cd9c51ba46c27b857680c6ce76
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-04-10 15:15:11 +0200

    completed sort function

--- Tut6-Sort/Tut6-Sort.cpp
+++ Tut6-Sort/Tut6-Sort.cpp
@@ -7,9 +7,27 @@ using namespace std;
  // create template 
 template <typename Tmpl8>
 
-// create a sort function  to sort an array
+// create a sort function  to sort an array given the array of integers and the array size
 
 void sortArr(Tmpl8 arrIntegers[], int arrSize){
+	int i, j;
+	Tmpl8 tempArr;
+	for (i = 0; i < arrSize-1; i++){
+
+		for (j = i+1; j < arrSize; j++){
+			// if array at position i is less than array at position i+1
+			if (arrIntegers[i] < arrIntegers[j]){
+
+
+				// swap the numbers to sort the array in descending order
+				tempArr = arrIntegers[j];
+				arrIntegers[j] = arrIntegers[i];
+				arrIntegers[i] = tempArr;
+			}
+
+		}
+
+	}
 
 }
 
